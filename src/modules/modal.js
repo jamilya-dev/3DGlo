beb775f651c0323000850368fbcd96e5cd4f3a12
jamilya-dev <omg.groups@yandex.ru>
2024-08-23 13:16:54 +0300

    доработка задания 20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,33 +3,36 @@ const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
   const popupClose = modal.querySelector(".popup-close");
   const windiwWidth = document.documentElement.clientWidth;
-  console.log(document.documentElement.clientWidth);
 
-  if (windiwWidth > 768) {
-    modal.style.opacity = "0";
-    modal.style.transition = "opacity 1s";
-  }
+  let count = 0;
+  let intervalId;
+
+  const animation = () => {
+    count++;
+    intervalId = requestAnimationFrame(animation);
+    if (count <= 50) {
+      let opacity = Number(modal.style.opacity);
+      opacity += 0.02;
+      modal.style.opacity = opacity;
+    } else {
+      cancelAnimationFrame(intervalId);
+      count = 0;
+    }
+  };
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
-      modal.style.display = "block";
       if (windiwWidth > 768) {
-        requestAnimationFrame(() => {
-          modal.style.opacity = "1";
-        });
+        modal.style.display = "block";
+        modal.style.opacity = "0";
+        animation();
       }
     });
   });
 
   popupClose.addEventListener("click", () => {
-    if (windiwWidth > 768) {
-      modal.style.opacity = "0";
-      setTimeout(() => {
-        modal.style.display = "none";
-      }, 500);
-    } else {
-      modal.style.display = "none";
-    }
+    modal.style.display = "none";
+    modal.style.opacity = "0";
   });
 };
 
