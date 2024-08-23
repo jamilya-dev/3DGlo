11f31eec247572e84609bcf9415045639d645639
jamilya-dev <omg.groups@yandex.ru>
2024-08-23 11:32:09 +0300

    правки урока 19

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import timer from "./modules/timer";
 // import two from "./modules/two";
-timer("22 august 2024");
+timer("24 august 2024");
 // two();
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devServer: {
     hot: true,
     static: {
-      directory: path.resolve(__dirname, ""),
+      directory: path.resolve(__dirname, "dist"),
       watch: true,
     },
   },
