eb565d6b8dd9eb15bc326c3ffad6520a3c809546
jamilya-dev <omg.groups@yandex.ru>
2024-08-27 14:34:33 +0300

    выполенено задание 24

---
+++ src/modules/calc.js
@@ -0,0 +1,40 @@
+const calc = (price = 100) => {
+  const calcBlock = document.querySelector('.calc-block');
+  const calcType = document.querySelector('.calc-type');
+  const calcSquare = document.querySelector('.calc-square');
+  const calcCount = document.querySelector('.calc-count');
+  const calcDay = document.querySelector('.calc-day');
+  const total = document.getElementById('total');
+
+  const countCalc = () => {
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = calcSquare.value;
+
+    let toralValue;
+    let calcCountValue = 1;
+    let calcDayValue = 1;
+
+    if (calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
+    }
+    if (calcDay.value && calcDay.value < 5) {
+      calcDayValue = 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      calcDayValue = 1.5;
+    }
+
+    if (calcType.value && calcSquare.value) {
+      toralValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+    } else {
+      toralValue = 0;
+    }
+    total.textContent = toralValue;
+  };
+
+  calcBlock.addEventListener('input', (e) => {
+    if (e.target === calcType || e.target === calcSquare || calcCount || e.target === calcDay) {
+      countCalc();
+    }
+  });
+};
+export default calc;
