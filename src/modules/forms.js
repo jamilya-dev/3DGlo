16a05ebfa42cb4e669480fcb56a2be42777d495f
jamilya-dev <omg.groups@yandex.ru>
2024-09-01 12:23:39 +0300

    правка задания 21

--- src/modules/forms.js
+++ src/modules/forms.js
@@ -1,7 +1,7 @@
 const forms = () => {
   const nameInputs = document.querySelectorAll('input[name=user_name]');
-  const phoneInputs = document.querySelectorAll('input[name=user_email]');
-  const emailInputs = document.querySelectorAll('input[name=user_phone]');
+  const phoneInputs = document.querySelectorAll('input[name=user_phone]');
+  const emailInputs = document.querySelectorAll('input[name=user_email]');
   const messageInputs = document.querySelector('input[name=user_message]');
   const calcInputs = document.querySelectorAll('.calc-block > input[type=text]');
 
